0ee7aea09c15aec0a158d95724a03b9d5572ae5f
teambuildnex-star <teambuildnex@gmail.com>
2026-04-17 03:23:23 +0530

    rebuild

---
+++ dist/assets/About-PcFEoKE3.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-glayuPVq.js";function e(){return o.jsxs("div",{children:[o.jsx("h1",{children:"About Us"}),o.jsx("p",{children:"Welcome to our about page!"})]})}export{e as default};
